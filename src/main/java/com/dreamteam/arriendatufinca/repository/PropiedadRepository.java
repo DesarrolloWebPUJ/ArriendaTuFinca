cd5def18ab489ffcfdf6d1f87a067486dd2d42a6
CaMoraG <camoraq83@gmail.com>
2024-10-11 01:05:52 -0500

    Prueba 2

--- src/main/java/com/dreamteam/arriendatufinca/repository/PropiedadRepository.java
+++ src/main/java/com/dreamteam/arriendatufinca/repository/PropiedadRepository.java
@@ -1,9 +1,19 @@
 package com.dreamteam.arriendatufinca.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.dreamteam.arriendatufinca.entities.Propiedad;
+import com.dreamteam.arriendatufinca.entities.Solicitud;
 
 public interface PropiedadRepository extends CrudRepository<Propiedad, Integer> {
+    @Query("SELECT s FROM Solicitud s WHERE s.propiedad.arrendador.id = :arrendadorId")
+    List<Solicitud> findByPropiedadArrendadorId(@Param("arrendadorId") Integer arrendadorId);
 
+    @Query("SELECT s FROM Solicitud s ORDER BY s.fechaCreacion DESC")
+    List<Solicitud> findTopByFechaCreacionDesc(Pageable pageable);
 }
